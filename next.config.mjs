fe251d523124ebdd951b68ea4ab6e81262fe4b98
hmdnprks <hamdan.prakoso@mail.ugm.ac.id>
2024-10-31 10:30:35 +0700

    fix: fix security headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const cspHeader = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline';
-  style-src 'self' 'unsafe-inline';
+  script-src 'self' 'unsafe-eval';
+  style-src 'self';
   img-src 'self' blob: data: https://*.tile.openstreetmap.org;
   font-src 'self' data:;
   object-src 'none';
@@ -25,7 +25,16 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
             value: cspHeader.replace(/\n/g, ''),
-          }
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Permissions-Policy',
+            value:
+              'geolocation=()',
+          },
         ]
       }
     ];
